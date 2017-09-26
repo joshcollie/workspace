699cd9d494790f2c94cb62318160e91042e0fda1
Josh Collie <joshcollie@gmail.com>
2017-09-26 15:25:03 +0100

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -1,1 +1,33 @@
+# Initialise workstation with @joshcollie environment
+# Install Xcode tools
+xcode-select --install
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update Homebrew packages
+brew update
+
+# Install Atom
+https://atom.io/download/mac
+
+# Install Git
+brew install git
+
+# Install Go
+brew install go --cross-compile-common
+# Install Go based applications
+go get github.com/ejcx/passgo
+
+# Install Opera
+# Download from http://www.opera.com/computer/thanks?ni=stable&os=mac
+
+# Install Postgres
+brew install postgres
+
+# Install Sass
+gem install sass
+
+# Install Sketch
+# Download from https://www.sketchapp.com/download/sketch.zip
 
